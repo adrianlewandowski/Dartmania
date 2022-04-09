22c10f7da28bccadf65bb985c82d7acc1b684b1a
Sassanek <adriano2250@gmail.com>
2022-04-09 14:59:07 +0200

    Adding View for Game

--- Dartmania/Dartmania/Dartmania/Views/GameViewPage.xaml.cs
+++ Dartmania/Dartmania/Dartmania/Views/GameViewPage.xaml.cs
@@ -16,5 +16,69 @@ namespace Dartmania.Views
         {
             InitializeComponent();
         }
+        private decimal firstNumner;
+        private string operatorName;
+        private int multiply;
+        private void BtnCommon_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            int score = Convert.ToInt32(LblResult.Text);
+            int CurrentThrow = Convert.ToInt32(button.Text);
+            if(multiply == 3)
+            {
+                CurrentThrow *= 3;
+            }else if(multiply == 2)
+            {
+                CurrentThrow *= 2;
+            }
+            score -= CurrentThrow;
+            LblResult.Text = Convert.ToString(score);
+            multiply = 1;
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            LblResult.Text = "0";
+            multiply = 1;
+            firstNumner = 0;
+        }
+
+        private void BtnX_Clicked(object sender, EventArgs e)
+        {
+            string number = LblResult.Text;
+            if (number != "0")
+            {
+                number = number.Remove(number.Length - 1, 1);
+                if (string.IsNullOrEmpty(number))
+                {
+                    LblResult.Text = "0";
+                }
+                else
+                {
+                    LblResult.Text = number;
+                }
+            }
+        }
+
+        private async void Btn_Multiply(object sender, EventArgs e)
+        {
+            try
+            {
+                var button = sender as Button;
+                if(button.Text == "TRIPLE")
+                {
+                    multiply = 3;
+                }
+                else
+                {
+                    multiply = 2;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+
     }
 }
