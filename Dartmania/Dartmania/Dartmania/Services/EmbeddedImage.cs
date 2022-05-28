033501b4f4a29c8e7ed8e0090d6880770035a2e3
Sassanek <adriano2250@gmail.com>
2022-05-28 16:25:38 +0200

    Creating board with clickevent

---
+++ Dartmania/Dartmania/Dartmania/Services/EmbeddedImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Dartmania.Services
+{
+    [ContentProperty(nameof(Source))]
+    public static class EmbeddedImage
+    {
+        public static string Source { get; set; }
+
+        public static object ProvideValue (IServiceProvider serviceProvider)
+        {
+            if(Source == null)
+            {
+                return null;
+            }
+            var imageSource = ImageSource.FromResource(Source, typeof(EmbeddedImage).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
--- Dartmania/Dartmania/Dartmania/ViewModels/DartboardViewModel.cs
+++ Dartmania/Dartmania/Dartmania/ViewModels/DartboardViewModel.cs
@@ -15,5 +15,6 @@ namespace Dartmania.ViewModels
         {
             Title = "Dartmania";
         }
+
     }
 }
